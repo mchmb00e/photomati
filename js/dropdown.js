ae34e8812a3d97361be3d1d2af4c7c8cfb627ead
Víctor Iskándar <vgutierrezdiban@protonmail.com>
2023-12-09 18:38:03 -0300

    Implementar botón siguiente

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,7 +1,8 @@
 const activarDropdown = () => {
 	$(".dropdown-content").hide();
 
-	$(".drop-div").on("click", function(){
+	$(".drop-div").on("click", function() {
+
 		var content = $(this).next();
 		// "!" = "No"
 		if(!content.is(":visible")){
@@ -10,5 +11,20 @@ const activarDropdown = () => {
 		} else {
 			content.slideUp();
 		}
+	});
+
+	$("#boton-siguiente").on("click", (e) => {
+		$(".dropdown-content").each(function() {
+			if ($(this).is(":visible") && $(this).next().next().length > 0) {
+				
+				$("#steps").animate({
+					scrollTop: $(this).offset().top - $("#showResult").offset().top + $("#steps").scrollTop()
+				}, 800);
+				$(this).slideUp();
+				$(this).next().next().slideDown();
+				
+				return false;
+			}
+		});
 	});
 }
